2d1b3a15c98e61f3a781675ee061239d6a1da749
ignaciosm <ignaciosm@gmail.com>
2018-12-20 16:30:18 -0500

    change override for fixed add to cart

--- app/overrides/add_wish_to_cart_form.rb
+++ app/overrides/add_wish_to_cart_form.rb
@@ -1,6 +1,13 @@
+# Deface::Override.new(
+#   virtual_path: 'spree/products/_product_price',
+#   name: 'add_wish_to_cart_form',
+#   insert_bottom: "[data-hook='product_price']",
+#   partial: 'spree/products/wishlist_form'
+# )
+
 Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'add_wish_to_cart_form',
-  insert_bottom: "[data-hook='cart_form']",
+  insert_after: "[data-hook='cart_form']",
   partial: 'spree/products/wishlist_form'
 )
